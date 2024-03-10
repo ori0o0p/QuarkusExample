51e673dadc090a0c4af0503b9b2bfb7c62fdbf69
101 <tmddnjs001133@dsm.hs.kr>
2024-03-10 16:36:15 +0900

    update Article.java

--- src/main/java/com/example/article/model/Article.java
+++ src/main/java/com/example/article/model/Article.java
@@ -1,21 +1,33 @@
 package com.example.article.model;
 
 import io.quarkus.mongodb.panache.reactive.ReactivePanacheMongoEntity;
+import io.smallrye.mutiny.Multi;
 import io.smallrye.mutiny.Uni;
 import jakarta.persistence.Id;
 import org.bson.types.ObjectId;
 
+import java.time.Duration;
+import java.util.Objects;
+
 public class Article extends ReactivePanacheMongoEntity {
 
     @Id
-    public ObjectId id;
+    private ObjectId id;
 
-    public String title;
+    private String title;
 
-    public String content;
+    private String content;
 
     public static Uni<Article> findArticleById(String id) {
         return findById(new ObjectId(id));
     }
-    
+
+    public static Uni<Void> save(Article article) {
+        return persist(article);
+    }
+
+    public static Multi<Article> findAllArticle() {
+        return streamAll();
+    }
+
 }
