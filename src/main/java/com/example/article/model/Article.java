b55550ab003472d75f0c16bc303362dac9bab0de
101 <tmddnjs001133@dsm.hs.kr>
2024-03-10 11:56:02 +0900

    add Article.java

--- src/main/java/com/example/article/model/Article.java
+++ src/main/java/com/example/article/model/Article.java
@@ -1,2 +1,21 @@
-package com.example.article.model;public class Article {
+package com.example.article.model;
+
+import io.quarkus.mongodb.panache.reactive.ReactivePanacheMongoEntity;
+import io.smallrye.mutiny.Uni;
+import jakarta.persistence.Id;
+import org.bson.types.ObjectId;
+
+public class Article extends ReactivePanacheMongoEntity {
+
+    @Id
+    public ObjectId id;
+
+    public String title;
+
+    public String content;
+
+    public static Uni<Article> findArticleById(String id) {
+        return findById(new ObjectId(id));
+    }
+    
 }
